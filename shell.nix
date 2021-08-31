e091749e47278f337737c09e341bb931b8aa0150
aylax zhou <26481009+aylax@users.noreply.github.com>
2021-09-01 00:25:14 +0800

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,32 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+mkShell {
+  shellHook = ''
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
+      pkgs.alsaLib
+      pkgs.udev
+      pkgs.vulkan-loader
+    ]}"
+  '';
+  buildInputs = [ 
+    cargo
+
+    # fast compiler
+    lld
+    clang
+
+    # bevy dep packages
+    pkgconfig
+    udev
+    alsaLib
+    #lutris
+    x11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    vulkan-tools
+    vulkan-headers
+    vulkan-loader
+    vulkan-validation-layers
+  ];
+}
