62cfafc2558b045afaa276f2d20bfb39f52e838a
aylax <zhoubye@foxmail.com>
2021-09-18 22:44:26 +0800

    new :: game snake -> add camera

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::Camera};
 
 fn main() {
-    App::build().add_plugins(DefaultPlugins).run();
+    App::build()
+        .add_startup_system(setup.system())
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn().insert(Camera::default());
 }
