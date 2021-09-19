4d5b574db9fdb1cdfee65e0667b749757f0d8bff
aylax <zhoubye@foxmail.com>
2021-09-19 23:37:33 +0800

    new :: game snake -> add window

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use bevy::{prelude::*, render::camera::Camera};
 
 fn main() {
     App::build()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(WindowDescriptor {
+            title: "Snake!".to_string(),
+            width: 600.0,
+            height: 400.0,
+            ..Default::default()
+        })
         .add_startup_system(setup.system())
         .add_startup_stage("game_setup", SystemStage::single(spawn_snake.system()))
         .add_plugins(DefaultPlugins)
