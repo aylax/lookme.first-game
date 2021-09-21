b3833535c413aec1deb692af759e62621dfbc096
aylax <zhoubye@foxmail.com>
2021-09-21 11:05:54 +0800

    new :: ecs -> use log system

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use bevy::prelude::*;
 
 fn main() {
     println!("[ ECS ]");
-    App::new().run();
+    App::new()
+    .add_system(log_system)
+    .run();
 }
 
 
@@ -15,3 +17,12 @@ struct Health(u8);
 // Entity
 #[derive(Default)]
 struct Human;
+
+
+fn log_system() {
+    trace!("very noisy");
+    debug!("helpful for debugging");
+    info!("helpful information that is worth printing by default");
+    warn!("something bad happened that isn't a failure, but thats worth calling out");
+    error!("something failed");
+}
