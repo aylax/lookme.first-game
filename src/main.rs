01b09686e4fee7c70ac1490ddb4cf3344578d0c4
aylax <zhoubye@foxmail.com>
2021-09-21 15:35:52 +0800

    new :: ecs -> define struct

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,26 @@ use bevy::prelude::*;
 fn main() {
     println!("[ ECS ]");
     App::new()
-    .add_system(log_system)
-    .run();
+        .add_startup_system(setup_system)
+        .add_system(print_system)
+        .run();
 }
 
-
 #[derive(Component)]
-struct Name;
+struct Name(String);
 
 #[derive(Component)]
 struct Health(u8);
 
-// Entity
-#[derive(Default)]
-struct Human;
+fn setup_system(mut commands: Commands) {
+    println!("[ ECS ] -- < setup >");
+    commands.spawn_bundle((Name(format!("Jade")), Health(60)));
+    commands.spawn_bundle((Name(format!("Judi")), Health(90)));
+    commands.spawn_bundle((Name(format!("None")), Health(40)));
+}
 
-
-fn log_system() {
-    trace!("very noisy");
-    debug!("helpful for debugging");
-    info!("helpful information that is worth printing by default");
-    warn!("something bad happened that isn't a failure, but thats worth calling out");
-    error!("something failed");
+fn print_system(query: Query<(&Name, &Health)>) {
+    for (name, health) in query.iter() {
+        println!("name: {}, health: {}", name.0, health.0);
+    }
 }
